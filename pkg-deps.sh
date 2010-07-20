b39bdb20c7cc2fa98baa710119f402a0a67e8837
Jens Petersen <petersen@fedoraproject.org>
2010-07-20 09:07:52 +0000

    add gtk2hs-buildtools to pkg-deps.sh

--- pkg-deps.sh
+++ pkg-deps.sh
@@ -13,7 +13,7 @@ cd .pkg-deps
 ghc-pkg dot --global | sed '$d' > pkgs.dot
 
 # check for binary deps too
-for i in alex cabal-install cpphs darcs ghc happy haskell-platform hedgewars hscolour kaya xmonad; do
+for i in alex cabal-install cpphs darcs ghc happy gtk2hs-buildtools haskell-platform hedgewars hscolour kaya xmonad; do
   PKG_THERE=yes
   PKG=`rpm -q --qf "%{name}-%{version}" $i` || PKG_THERE=no
   if [ "$PKG_THERE" = "yes" ]; then
