09c03732d541ebae4aebe223fa7f1ae0e352d769
Jens Petersen <petersen@fedoraproject.org>
2016-11-20 21:03:21 +0900

    improve libraries-versions.sh: exclude ghc*
    - s/-/_/

--- libraries-versions.sh
+++ libraries-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+GHCVER=$(basename $PWD | sed -e "s/ghc-//")
+
 if [ ! -d libraries ]; then
     echo Is CWD a ghc source tree?
     exit 1
@@ -7,4 +9,4 @@ fi
 
 cd libraries
 
-grep -i ^version: Cabal/Cabal/Cabal.cabal */*.cabal | grep -v -e "\(Win32\|gmp.old\|gmp2\|integer-simple\)" | sed -e "s!/.*: \+!_ver !"
+grep -i ^version: Cabal/Cabal/Cabal.cabal */*.cabal | grep -v -e "\(Win32\|gmp.old\|gmp2\|integer-simple\|$GHCVER\)" | sed -e "s!/.*: \+!_ver !" -e "s/-/_/g"
